958b3b3e83e51af4396b53804f6711e6cde10dff
Marumasa <77374813+malken21@users.noreply.github.com>
2024-11-03 19:35:15 +0900

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,61 +1,53 @@
 
-window.onload = () => {
-    updateTime();
-    updateTimetable()
+window.onload = async () => {
+    let todayTimetable = await getTodayTimetable();
+    const event = () => {
+        updateTime();
+        updateTimetable(todayTimetable);
+    }
+    event();
+    // 10 秒毎に実行
+    setInterval(event, 10000);
 }
 
 // 現在時刻を表示する関数
 function updateTime() {
-    const event = () => {
-        document.getElementById('CurrentTime').textContent = toStringTime(new Date());
-    }
-    event();
-    // 10 秒毎に実行
-    setInterval(event, 10000);
+    document.getElementById('CurrentTime').textContent = toStringTime(new Date());
 }
 
-
 // 時刻表を表示する関数
-async function updateTimetable() {
+async function updateTimetable(todayTimetable) {
 
-    let todayTimetable = await getTodayTimetable();
+    const now = new Date();
 
-    const event = () => {
+    const displayLines = getDisplayLines(todayTimetable, 2);
+    displayLines.forEach((line_date, index) => {
+        // 現在時刻との差を ミリ秒単位で引き算 して 単位を分に変換して少数を切り捨て
+        const diffIn_min = Math.floor((line_date.getTime() - now.getTime()) / (1000 * 60));
 
-        const now = new Date();
+        let line_text = `あと ${diffIn_min}分 `;
+        let line_text_class = "";
 
-        const displayLines = getDisplayLines(todayTimetable, 2);
-        displayLines.forEach((line_date, index) => {
-            // 現在時刻との差を ミリ秒単位で引き算 して 単位を分に変換して少数を切り捨て
-            const diffIn_min = Math.floor((line_date.getTime() - now.getTime()) / (1000 * 60));
+        if (diffIn_min > WalkMinutes) {
+            line_text += "余裕で間に合います";
+            line_text_class = "notice notice-leeway";
+        } else if (diffIn_min > RunMinutes) {
+            line_text += "歩いても間に合います";
+            line_text_class = "notice notice-walk";
+        } else {
+            line_text += "走れば間に合います";
+            line_text_class = "notice notice-run";
+        }
 
-            let line_text = `あと ${diffIn_min}分 `;
-            let line_text_class = "";
+        // 電車のメッセージを設定
+        const line_text_elm = document.getElementById(`line_text_${index}`);
+        line_text_elm.textContent = line_text;
+        line_text_elm.className = line_text_class;
 
-            if (diffIn_min > WalkMinutes) {
-                line_text += "余裕で間に合います";
-                line_text_class = "notice notice-leeway";
-            } else if (diffIn_min > RunMinutes) {
-                line_text += "歩いても間に合います";
-                line_text_class = "notice notice-walk";
-            } else {
-                line_text += "走れば間に合います";
-                line_text_class = "notice notice-run";
-            }
-
-            // 電車のメッセージを設定
-            const line_text_elm = document.getElementById(`line_text_${index}`);
-            line_text_elm.textContent = line_text;
-            line_text_elm.className = line_text_class;
-
-            // 電車の時刻を設定
-            const line_time_elm = document.getElementById(`line_time_${index}`);
-            line_time_elm.textContent = toStringTime(line_date);
-        });
-    }
-    event();
-    // 10 秒毎に実行
-    setInterval(event, 10000);
+        // 電車の時刻を設定
+        const line_time_elm = document.getElementById(`line_time_${index}`);
+        line_time_elm.textContent = toStringTime(line_date);
+    });
 }
 
 // 今日の時刻表を取得する関数
@@ -84,19 +76,18 @@ function getDisplayLines(todayTimetable, length) {
 
     const hoursList = Object.keys(todayTimetable);
     hoursList.some(hours => {
-        return () => {
-            if (hours >= ref_hours) {
-                const minutesList = todayTimetable[hours];
-                minutesList.some(minutes => {
-                    if (minutes >= ref_minutes || ref_hours < hours) {
-                        const tmpDate = new Date();
-                        tmpDate.setHours(hours);
-                        tmpDate.setMinutes(minutes);
-                        recentTimeList.push(tmpDate);
-                    }
-                    if (recentTimeList.length >= length) return true;
-                });
-            }
+        if (hours >= ref_hours) {
+            const minutesList = todayTimetable[hours];
+            return minutesList.some(minutes => {
+                if (minutes >= ref_minutes || ref_hours < hours) {
+                    const tmpDate = new Date();
+                    tmpDate.setHours(hours);
+                    tmpDate.setMinutes(minutes);
+                    recentTimeList.push(tmpDate);
+                }
+                if (recentTimeList.length >= length) return true;
+                return false;
+            });
         }
     });
     return recentTimeList;

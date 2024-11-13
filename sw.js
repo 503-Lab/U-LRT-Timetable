fb33eea479bb860b9be4cd45dfa9295d0a6cf147
Marumasa <77374813+malken21@users.noreply.github.com>
2024-11-13 15:45:49 +0900

    2回 Webアプリを読み込まないと 変更が反映されない問題を修正

--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@ const CACHE_NAME = 'file-cache-v1';
 // キャッシュするファイルのリスト
 const FILES_TO_CACHE = [
     './',
+    './manifest.json',
     './index.html',
     './style.css',
     './script/main.js',
@@ -26,34 +27,40 @@ self.addEventListener('install', (event) => {
 
 // フェッチイベントをリスニング
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                // キャッシュが見つかった場合はそのレスポンスを返す
-                if (response) {
-                    // ネットワークからもリクエストを行い、結果をキャッシュを更新する
-                    fetch(event.request).then((networkResponse) => {
-                        // レスポンスが有効な場合のみキャッシュを更新
-                        if (networkResponse && networkResponse.status === 200) {
-                            const networkResponseClone = networkResponse.clone();
-                            caches.open(CACHE_NAME).then((cache) => {
-                                cache.put(event.request, networkResponseClone);
-                            });
-                        }
-                    });
-                    return response; // キャッシュからのレスポンスを返す
-                }
-                // キャッシュが見つからない場合はネットワークから取得
-                return fetch(event.request).then((networkResponse) => {
-                    // 取得したレスポンスをキャッシュに追加
-                    if (networkResponse && networkResponse.status === 200) {
-                        const networkResponseClone = networkResponse.clone();
-                        caches.open(CACHE_NAME).then((cache) => {
-                            cache.put(event.request, networkResponseClone);
-                        });
-                    }
-                    return networkResponse; // ネットワークからのレスポンスを返す
-                });
-            })
-    );
+    event.respondWith(caches.match(event.request).then((response) => {
+        // キャッシュが見つかった場合
+        if (response) {
+            // ネットワークにリクエストを送信する
+            return fetch(event.request).then((networkResponse) => {
+                // キャッシュを更新
+                putCache(event, networkResponse);
+                // ネットワークからのレスポンスを返す
+                return networkResponse;
+            }).catch(() => {
+                // オフラインの場合はキャッシュのレスポンスを返す
+                return response;
+            });
+        }
+        // キャッシュが見つからない場合はネットワークから取得
+        return fetch(event.request).then((networkResponse) => {
+            // キャッシュを更新
+            putCache(event, networkResponse);
+            // ネットワークからのレスポンスを返す
+            return networkResponse;
+        }).catch(() => {
+            // オフラインでキャッシュもない場合
+            return new Response("", { status: 404 });
+        });
+    }));
 });
+
+function putCache(event, networkResponse) {
+    // レスポンスが有効な場合のみキャッシュを更新
+    if (networkResponse && networkResponse.status === 200) {
+        const networkResponseClone = networkResponse.clone();
+        // 複製したレスポンスをキャッシュに追加
+        caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponseClone);
+        });
+    }
+}

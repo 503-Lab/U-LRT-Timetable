9b0a74a45467b4693f43d1318e2122eedf200436
Marumasa <77374813+malken21@users.noreply.github.com>
2024-11-14 19:21:40 +0900

    通信環境が悪い状況での読み込み速度を向上

--- sw.js
+++ sw.js
@@ -29,27 +29,29 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
     event.respondWith(caches.match(event.request).then(async (cacheResponse) => {
         try {
-            // ネットワークにリクエストを送信
-            const networkResponse = await fetch(event.request);
-            // キャッシュの更新を試みる
-            tryPutCache(event, networkResponse, cacheResponse);
-            // ネットワークからのレスポンスを返す
-            return networkResponse;
-        } catch {
             // キャッシュが見つかった場合はキャッシュのレスポンスを返す
-            // そうでなければ ステータスコード 404 の空の文字列を返す
-            return cacheResponse ? cacheResponse : new Response("", { status: 404 });
+            if (cacheResponse) {
+                // キャッシュの更新を試みる
+                tryPutCache(event);
+                return cacheResponse;
+            } else {
+                // そうでなければネットワークのレスポンスを返す
+                return fetch(event.request);
+            }
+        } catch {
+            // ステータスコード 404 の空の文字列を返す
+            return new Response("", { status: 404 });
         }
     }));
 });
 
-function tryPutCache(event, networkResponse, cacheResponse) {
+async function tryPutCache(event) {
+    const networkResponse = await fetch(event.request);
     // キャッシュされる設定のリクエストで、レスポンスが有効な場合のみキャッシュを更新
-    if (cacheResponse && networkResponse && networkResponse.status === 200) {
-        const networkResponseClone = networkResponse.clone();
-        // 複製したレスポンスをキャッシュに追加
+    if (networkResponse && networkResponse.status === 200) {
+        // ネットワークのレスポンスをキャッシュに追加
         caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, networkResponseClone);
+            cache.put(event.request, networkResponse);
         });
     }
 }
